390c9f9587af5a45f86fe12b1ab86694b6d7dda5
sunil-karki <sunilkarki320@gmail.com>
2021-01-15 23:54:35 +0545

    Init for File Uploading and Serving

--- BackSideServer/main.go
+++ BackSideServer/main.go
@@ -8,10 +8,15 @@ import (
 	"os/signal"
 	"time"
 
+	"./files"
 	"./handlers"
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 )
+
+var bindAddress = env.String("BIND_ADDRESS", false, ":9090", "Bind address for the server")
+var logLevel = env.String("LOG_LEVEL", false, "debug", "Log output level for the server [debug, info, trace]")
+var basePath = env.String("BASE_PATH", false, "./imagestore", "Base path to save images")
 
 func main() {
 	// http.HandleFunc("/", handler)
@@ -19,6 +24,20 @@ func main() {
 	// hh := handlers.NewAbout(l)
 	ph := handlers.NewProducts(l)
 
+	//////////Section For file uploading and serving//////////////////////////////////////////////
+	// create the storage class, use local storage
+	// max filesize 5MB
+	stor, err := files.NewLocal(*basePath, 1024*1000*5)
+	if err != nil {
+		l.Error("Unable to create storage", "error", err)
+		os.Exit(1)
+	}
+
+	// create the another handlers.
+	fh := handlers.NewFiles(stor, l)
+	//////////////////////////////////////////////
+
+	// creating a new serve mux and registering the handlers
 	smux := mux.NewRouter()
 	// smux.Handle("/", ph)
 
@@ -33,6 +52,11 @@ func main() {
 	postRouter := smux.Methods(http.MethodPost).Subrouter()
 	postRouter.HandleFunc("/", ph.AddProduct)
 	postRouter.Use(ph.MiddlewareValidateProduct)
+
+	///////// Section For file uploading and serving//////////////////////////////////////////////
+	ph := sm.Methods(http.MethodPost).Subrouter()
+	ph.HandleFunc("/images/{id:[0-9]+}/{filename:[a-zA-Z]+\\.[a-z]{3}}", fh.ServeHTTP)
+	///////////////////////////////////////////////
 
 	// Solves Cross Origin Access Issue
 	c := cors.New(cors.Options{
